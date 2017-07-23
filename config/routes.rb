3a8bda6820c84a09a08b6081b12334a596454119
ilentt <ilentt@ilenlab.com>
2017-07-23 13:22:46 +0700

    CRUD for Category and Publisher

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :books
   resources :publishers
   resources :categories
+  get '/categories/new', to: 'categories#new'
+  get '/publishers/new', to: 'publishers#new'
   get 'home/index'
   root :to => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
