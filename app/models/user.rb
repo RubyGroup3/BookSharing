05a2a2904e1d29e2908f0c80bb92caa6de1f65d9
vuongtrieulk <vuongtrieulk@gmail.com>
2017-07-29 15:06:18 +0700

    update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :book_deals
+  validates :username, presence: true
 end
