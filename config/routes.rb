47bab390257e81ba645bd453a2dbff4fcd086587
vuongtrieulk <vuongtrieulk@gmail.com>
2017-07-29 11:31:03 +0700

    Add Admin function

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   resources :book_deals
   devise_for :users
   resources :books
-  resources :publishers
-  resources :categories
   get '/categories/new', to: 'categories#new'
   get '/publishers/new', to: 'publishers#new'
+  get '/admin', to: 'admin#index'
+  scope '/admin' do
+    resources :publishers
+    resources :categories
+  end
   get 'home/index'
   root :to => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
