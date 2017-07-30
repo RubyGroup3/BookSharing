23415222febed45ecc9869afb5efc0b0d9330532
ilentt <ilentt@ilenlab.com>
2017-07-30 17:37:36 +0700

    update all chage

---
+++ app/assets/javascripts/administrate/application.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require selectize
+//= require moment
+//= require datetime_picker
+//= require bootstrap_sb_admin_base_v2
+//= require_tree .
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@user = User.create(username: "admin", email: "admin@mail.com", password: "123456", admin: true)
+
+Category.create([{name: "Fiction"}, {name: "History"}, {name: "Mystery"}, {name: "Comic"}])
+Publisher.create(name: "test")
+(1..5).each do
+	Category.all.each do |c|
+
+		book = Book.create(
+			name: Faker::Book.title,
+			image: "https://images-na.ssl-images-amazon.com/images/I/51I0SwJTJCL._SX342_BO1,204,203,200_.jpg",
+			pages: 200,
+			category: c,
+			publisher: Publisher.first
+		)
+
+		BookDeal.create(
+			book: book,
+			user: @user,
+			description: Faker::Lorem.paragraph
+		)
+
+	
+	end
+
+end
+
+
+
+
+	
