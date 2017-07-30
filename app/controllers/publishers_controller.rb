f927806238b884049601a87648fe48b5d17e01a7
ilentt <ilentt@ilenlab.com>
2017-07-30 10:24:31 +0700

    Add Category and Publisher into Book

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -28,7 +28,8 @@ class PublishersController < ApplicationController
 
     respond_to do |format|
       if @publisher.save
-        format.html { redirect_to @publisher, notice: 'Publisher was successfully created.' }
+        #format.html { redirect_to @publisher, notice: 'Publisher was successfully created.' }
+        format.html { redirect_to admin_path, notice: 'Publisher was successfully created.' }
         format.json { render :show, status: :created, location: @publisher }
       else
         format.html { render :new }
@@ -42,7 +43,8 @@ class PublishersController < ApplicationController
   def update
     respond_to do |format|
       if @publisher.update(publisher_params)
-        format.html { redirect_to @publisher, notice: 'Publisher was successfully updated.' }
+        #format.html { redirect_to @publisher, notice: 'Publisher was successfully updated.' }
+        format.html { redirect_to admin_path, notice: 'Publisher was successfully updated.' }
         format.json { render :show, status: :ok, location: @publisher }
       else
         format.html { render :edit }
@@ -56,7 +58,8 @@ class PublishersController < ApplicationController
   def destroy
     @publisher.destroy
     respond_to do |format|
-      format.html { redirect_to publishers_url, notice: 'Publisher was successfully destroyed.' }
+      #format.html { redirect_to publishers_url, notice: 'Publisher was successfully destroyed.' }
+      format.html { redirect_to admin_path, notice: 'Publisher was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
