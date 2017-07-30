7d81db28fdf47baebe444331feea5ca885594030
ilentt <ilentt@ilenlab.com>
2017-07-30 17:14:47 +0700

    Solve conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
-  def index
-<<<<<<< HEAD
+	def index
 		@books = Book.all
 		@deals = BookDeal.all
 	end
-=======
-  end
->>>>>>> 6baf579e6a8dc51a005efe3cd1b53d5c2c2dfddc
 end
